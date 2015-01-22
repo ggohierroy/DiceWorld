285e6f5c5e985f9579d26c3d023f013de8b82404
Gabriel Gohier-Roy <ggohierroy@hotmail.com>
2015-01-21 22:12:51 -0500

    Add missing files and database script

---
+++ DiceWorld/DTOs/Meta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiceWorld.DTOs
+{
+    public class Meta
+    {
+        public int Total { get; set; }
+    }
+}
