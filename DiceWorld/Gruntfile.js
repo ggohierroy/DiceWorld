4b654a73d4869f993a7b1d6eb207af333fe29694
Gabriel Gohier-Roy <ggohierroy@hotmail.com>
2015-02-08 09:55:35 -0500

    Added hbs files for compilation, but it doesn't fucking work so fuck it for now

--- DiceWorld/Gruntfile.js
+++ DiceWorld/Gruntfile.js
@@ -3,22 +3,32 @@
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        emberhandlebars: {
-            compile: {
-                options: {
-                    templateName: function (sourceFile) {
-                        var newSource = sourceFile.replace('Scripts/Templates/', '');
-                        newSource = newSource.replace('_', '/');
-                        return newSource.replace('.hbs', '');
-                    }
-                },
-                files: [
-                  'Scripts/Templates/*.hbs'
-                ],
-                dest: 'Scripts/Templates/templates.js'
-            }
-        }
     });
 
-    grunt.loadNpmTasks('grunt-ember-template-compiler');
+    grunt.registerTask('ember-template-compiler', 'Compiles ember templates', function () {
+        var glob = require('glob');
+        var fs = require('fs');
+        var compiler = require('./bower_components/ember/ember-template-compiler');
+
+        var formatName = function (sourcefile) {
+            var newSource = sourcefile.replace('Scripts/Templates/', '');
+            newSource = newSource.replace('_', '/');
+            return newSource.replace('.hbs', '');
+        }
+
+        var pattern = 'Scripts/Templates/*.hbs';
+
+        var output = "";
+        glob.sync(pattern).forEach(function (pathMatch) {
+            var contents = fs.readFileSync(pathMatch, { encoding: 'utf8' });
+            contents = contents.replace(/\r\n/g, '\n');
+            var compiled = compiler.precompile(contents, false);
+            output += "Ember.TEMPLATES['" + formatName(pathMatch) + "'] = Ember.Handlebars.template(" + compiled + ");";
+        });
+
+        fs.writeFileSync('./Scripts/Templates/templates.js', output, { encoding: 'utf8' });
+    });
+
+    // the default task can be run just by typing "grunt" on the command line
+    grunt.registerTask('default', ['ember-template-compiler']);
 };
