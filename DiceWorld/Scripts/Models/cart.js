d81139dbddeaaf450e4507e913a616cce962df19
Gabriel Gohier-Roy <ggohierroy@hotmail.com>
2015-02-15 13:39:55 -0500

    cart items are now saved in session storage

--- DiceWorld/Scripts/Models/cart.js
+++ DiceWorld/Scripts/Models/cart.js
@@ -1,4 +1,7 @@
 ﻿App.Cart = DS.Model.extend({
+
+    itemsKey: 'DiceWorldCartItems',
+
     cartItems: DS.hasMany('cartItem', { async: true }),
     user: DS.belongsTo('user'),
 
@@ -6,5 +9,12 @@
         return this.get('cartItems').reduce(function(previousValue, item) {
             return (previousValue || 0) + item.get('quantity') * item.get('boardGame.price');
         });
-    }.property('cartItems.@each.quantity')
+    }.property('cartItems.@each.quantity'),
+
+    cartItemAdded: function () {
+        var items = this.get('cartItems').map(function(item) {
+            return { quantity: item.get('quantity'), boardGame: item.get('boardGame.id') };
+        });
+        sessionStorage[this.get('itemsKey')] = JSON.stringify(items);
+    }.observes('cartItems.@each.quantity')
 });
--- DiceWorld/Scripts/Models/session.js
+++ DiceWorld/Scripts/Models/session.js
@@ -1,35 +1,80 @@
 ﻿App.Session = Ember.Object.extend({
 
-    key: 'DiceWorldSession',
+    sessionKey: 'DiceWorldSession',
+    itemsKey: 'DiceWorldCartItems',
 
     init: function () {
 
-        this.get('token');
-        
-        var session = sessionStorage[this.get('key')];
-
+        // Fetch session
+        var session = sessionStorage[this.get('sessionKey')];
         if (session) {
             session = JSON.parse(session);
             this.set('token', session.token);
             this.set('userId', session.userId);
-            this.set('username', session.username);
+        }
+        
+        // Fetch cart items
+        var cartItems = sessionStorage[this.get('itemsKey')];
+        if (cartItems) {
+            cartItems = JSON.parse(cartItems);
+            this.set('cartItems', cartItems);
         }
     },
+    
+    create: function(store) {
 
+        var resolve = Ember.RSVP.resolve();
+
+        // If we have a userId saved up, we can try to load it from the server
+        // to get the cart and orders data
+        var userId = this.get('userId');
+        if (userId) {
+            var self = this;
+            return this.store.find('user', userId).then(function (user) {
+                // success
+                self.set('user', user);
+                return resolve;
+            }, function() {
+                // fail (most probably because token expired)
+                self.createAnonymousUser(store);
+                return resolve;
+            });
+        }
+
+        this.createAnonymousUser(store);
+        return resolve;
+    },
+
+    cartItems: null,
     token: null,
-    username: null,
     userId: null,
     user: null,
 
     signOut: function() {
-        this.set('token', null);
-        this.set('userId', null);
-        this.set('user', null);
-        this.set('username', null);
+        this.setProperties({ token: null, userId: null, user: null, cartItems: null });
+
+        sessionStorage.removeItem(this.get('sessionKey'));
+        sessionStorage.removeItem(this.get('itemsKey'));
     },
 
     createAnonymousUser: function (store) {
+        // Create the cart
         var cart = store.createRecord('cart');
+        // Create the cart items
+        var cartItems = this.get('cartItems');
+        if (cartItems) {
+            cartItems.forEach(function (item) {
+                store.find('boardGame', item.boardGame).then(function (boardGame) {
+                    var cartItem = store.createRecord('cartItem', {
+                        cart: cart,
+                        quantity: item.quantity,
+                        boardGame: boardGame
+                    });
+                    cart.get('cartItems').pushObject(cartItem);
+                });
+            });
+        }
+        // Create the user
         this.set('user', store.createRecord('user', {
             anonymous: true,
             name: 'anonymous',
@@ -38,26 +83,24 @@
     },
 
     tokenOrUserChanged: function () {
-        Ember.run.once(this, 'saveSession');
+        this.saveSession();
     }.observes('token'),
 
     userChanged: function () {
         var user = this.get('user');
-        this.setProperties({ userId: user.id, username: user.get('name') });
+        if(user)
+            this.set('userId', user.id);
         this.saveSession();
     }.observes('user'),
 
     saveSession: function () {
+
         var userId = this.get('user.id');
         var token = this.get('token');
-        var username = this.get('user.name');
 
-        var key = this.get('key');
-
-        if (token) {
-            sessionStorage[key] = JSON.stringify({ token: token, username: username, userId: userId });
-        } else {
-            sessionStorage.removeItem(key);
+        if (token && userId) {
+            var key = this.get('sessionKey');
+            sessionStorage[key] = JSON.stringify({ token: token, userId: userId });
         }
     }
 })
